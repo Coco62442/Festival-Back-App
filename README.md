1c33430ee8e58cca07f7e956a04d97e07e79b8d0
laulilaura <laurabenaiton@gmail.com>
2023-02-26 02:27:18 +0100

    mise ne place du middleware CORS pour les requêtes depuis le front

--- README.md
+++ README.md
@@ -72,3 +72,10 @@ Nest is an MIT-licensed open source project. It can grow thanks to the sponsors 
 
 Nest is [MIT licensed](LICENSE).
 # Festival-Back-App
+
+
+### installation
+npm install bcrypt
+npm install class-validator
+## requetes avec axios
+npm install cors --save
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
+
+/*
+async function bootstrap() {
+  var cors = require('cors');
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+  app.use(cors({
+    origin: ['http://localhost:3001/zones', 'http://localhost:3001'] // Autorise les requêtes de http://example.com et http://localhost:3000
+  }));
+}
+bootstrap();
+*/
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  
+  // Ajouter le middleware CORS ici
+  app.use(cors({
+    origin: 'http://localhost:3001' // Permet uniquement les requêtes de http://localhost:3001
+  }));
+  
   await app.listen(3000);
 }
 bootstrap();
